828e2b6ed8c036322b597cb406422cf3a2e86a30
D-Vaillant <dvaillant@gmail.com>
2015-07-13 21:47:32 -0700

    C implementation of selection sort Also: python implementation cleaned up.

--- python/selection_sort.py
+++ python/selection_sort.py
@@ -1,16 +1,13 @@
-#!/usr/bin/env python3
+cat#!/usr/bin/env python3
 ''' selection_sort.py: Implements the selection sort algorithm. '''
 
 __author__ = "David Vaillant"
 __credits__ = "CLRS"
 
 def main(array):
-    for j in range(len(array)-1):
-        key = array[j]
-        for k in range(j+1, len(array)):
-            if array[k] < key:
-                temp = key
-                key = array[k]
-                array[k] = temp
+    for j, key in enumerate(array):
+        for laterVal in array[j+1:]:
+            if laterVal < key:
+                key, laterVal = laterVal, key
         array[j] = key
     return array
