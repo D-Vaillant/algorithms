f29efb2f2065837391e57fb721b4bab5d87c089a
D-Vaillant <dvaillant@gmail.com>
2015-08-01 11:04:06 -0700

    Implemented Stack in Python.

---
+++ python/data_structures/stack.py
@@ -0,0 +1,23 @@
+""" stack.py
+        Simple implementation of stacks in Python. """
+        
+class Stack:
+    def __init__(self):
+        self._buffer = []
+        
+    def isEmpty(self):
+        return self._buffer == []
+        
+    def push(self, x):
+        self._buffer.append(x)
+        
+    def pop(self):
+        return self._buffer.pop()
+        
+    def peek(self):
+        return self._buffer[-1]
+        
+    def size(self):
+        return len(self._buffer)
+        
+    
